98b0519fe4445d873b497c56664ad07058152701
chota5511 <quannnl92@gmail.com>
2022-02-28 03:45:50 +0000

    add call back

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const pg = require('pg');
 const json_utilities = require('./json_utilities.js');
 const aws_utilities = require('./aws_utilities.js');
 const utilities = require('./utilities.js');
+const sleep = require('system-sleep');
 
 const configureKey = process.env['configureKey'];
+const configureFile = JSON.parse(aws_utilities.s3Read('acbs-test-data',configureKey));
 const dbUserName = process.env['dbUserName'];
 const dbHost = process.env['dbHost'];
 const dbName = process.env['dbName'];
@@ -13,92 +15,121 @@ const awsAccessID = process.env["awsAccessID"];
 const awsSecretKey = process.env["awsSecretKey"];
 const awsEkycBucket = process.env["awsEkycBucket"];
 
-const configureFile = JSON.parse(aws_utilities.s3Read('acbs-test-data',configureKey));
-
 const result = {
   "Start datetime": '',
   "End datetime": '',
   "Execution": {
     "Query": '',
     "Respond query": '',
-    "Status": 'Failed'
+    "Status": ''
   }
 }
 
-result["Start datetime"] = new Date();
+function main() {
+  result["Start datetime"] = new Date();
+  console.log(`Start getting data: ${result["Start datetime"]}`);
 
-console.log(configureFile);
+  //Create client connection
+  const connection = {
+    user: dbUserName,
+    host: dbHost,
+    database: dbName,
+    password: dbPassword,
+    port: 5234
+  };
+  var client = new pg.Client(connection);
 
-//Create client connection
-const connection = {
-  user: dbUserName,
-  host: dbHost,
-  database: dbName,
-  password: dbPassword,
-  port: 5234
-};
-var client = new pg.Client(connection);
+  //Open connection
+  client.connect();
 
-//Open connection
-client.connect();
-
-//Begin getting data
-client.query(`SELECT MAX(updated_at)::TEXT FROM ekyc_customer`).then((max_updated_at,err) => {            //Run query to get lasted updated data date time
-  if (err) {
-    console.log(err);
-    client.end();                                                                                         //Close connection if err
-    result["End datetime"] = new Date();
-    console.log(JSON.stringify(result));;
-  }
-  else {
-    var updatedAt = max_updated_at['rows'][0]['max'];
-    console.log('Getting data at: ' + updatedAt);
-
-    //Build fill all data from last time run (if ekyc_updated_at is null then crawl all data)
-    var last_run_query_str = '';
-    if (configureFile["ekyc_updated_at"] != '') {
-      last_run_query_str = `AND updated_at > '${configureFile["ekyc_updated_at"]}'::TIMESTAMP`
+  //Begin getting data
+  client.query(`SELECT MAX(updated_at)::TEXT FROM ekyc_customer`).then((max_updated_at,err) => {            //Run query to get lasted updated data date time
+    if (err) {
+      console.error(err);
+      result["Execution"]['Failed reason'] = err;
+      result["End datetime"] = new Date();
+      result['Execution']["Status"] = 'Failed';
+      client.end();                                                                                         //Close connection if err
     }
-    var query_str = `SELECT * FROM ekyc_customer WHERE updated_at <= '${updatedAt}'::TIMESTAMP ${last_run_query_str}`;
-    result["Execution"]["Query"] = query_str;
-    
-    client.query(query_str).then((data_res,err1) => {                                                        //Run query for gather data
-      if (err1) {
-        console.log(err1);
-        client.end();
-        result["End datetime"] = new Date();
-        console.log(JSON.stringify(result));;
-      }
-      else {
-        var fields = [];
-        var data = '';
+    else {
+      var updatedAt = max_updated_at['rows'][0]['max'];
+      console.log('Getting data at: ' + updatedAt);
 
-        //Create table schema (fields)
-        for (var i in data_res["fields"])
-        {
-          fields.push(JSON.parse(JSON.stringify(data_res["fields"][i]["name"])).toString());
+      //Build fill all data from last time run (if ekyc_updated_at is null then crawl all data)
+      var last_run_query_str = '';
+      if (configureFile["ekyc_updated_at"] != '') {
+        last_run_query_str = `AND updated_at > '${configureFile["ekyc_updated_at"]}'::TIMESTAMP`
+      }
+      var query_str = `SELECT * FROM ekyc_customer WHERE updated_at <= '${updatedAt}'::TIMESTAMP ${last_run_query_str}`;
+      result["Execution"]["Query"] = query_str;
+
+      client.query(query_str).then((data_res,err1) => {                                                        //Run query for gather data
+        if (err1) {
+          console.error(err1);
+          result["End datetime"] = new Date();
+          result["Execution"]["Status"] = 'Failed';
+          result["Execution"]['Failed reason'] = err;
+          client.end();
         }
+        else {
+          var fields = [];
+          var data = '';
 
-        result["Respond query"] = data_res;
+          //Create table schema (fields)
+          for (var i in data_res["fields"])
+          {
+            fields.push(JSON.parse(JSON.stringify(data_res["fields"][i]["name"])).toString());
+          }
 
-        //Push data to a CSV file
-        data = json_utilities.jsonToCsv(data_res["rows"],fields)
+          result["Execution"]["Respond query"] = data_res;
 
-        //Save CSV file to S3
-        aws_utilities.s3Upload(data,awsEkycBucket,`ekyc_customer_${utilities.today()}.csv`);
+          //Push data to a CSV file
+          data = json_utilities.jsonToCsv(data_res["rows"],fields);
 
-        //Update config file
-        configureFile["ekyc_updated_at"] = updatedAt;
-        aws_utilities.s3Upload(JSON.stringify(configureFile),awsEkycBucket,configureKey);
+          //Save CSV file to S3
+          aws_utilities.s3Upload(data,awsEkycBucket,`ekyc_customer_${utilities.today()}.csv`);
 
-        //Return result
-        result["End datetime"] = new Date();
-        result["Execution"]["Status"] = 'Success';
-        console.log(JSON.stringify(result));
-        
-        //Close connection
-        client.end();
-      }
-    });
-  }
-});
+          //Update config file
+          configureFile["ekyc_updated_at"] = updatedAt;
+          aws_utilities.s3Upload(JSON.stringify(configureFile),awsEkycBucket,configureKey);
+
+          //Return result
+          result["End datetime"] = new Date();
+          result["Execution"]["Status"] = 'Success';
+          //console.log(JSON.stringify(result));
+
+          //Close connection
+          client.end();
+        }
+      });
+    }
+  });
+}
+
+exports.handler = (event,context,callback) => {
+  //Run main function
+  main();
+
+  var timeout = configureFile["Timeout"] ?? 15;
+
+  //Request timeout!!!
+  do {
+    if(result["Execution"]['Status'] != '') {
+      break;
+    }
+    else if (timeout <= 0) {
+      result["Execution"]['Status'] = 'Failed';
+      result["Execution"]['Failed reason'] = 'Request timeout!!!';
+      console.log(`End getting data: ${result["End datetime"]}`);
+      process.exit();
+      break;
+    }
+    else {
+      sleep(1000);
+      timeout--;
+    }
+  } while (true);
+  console.log(`End getting data: ${result["End datetime"]}`);
+  console.log(JSON.stringify(result));
+  callback(null,callback);
+}

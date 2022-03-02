dc54bab270fdc9d29cebd21a20a181caf7df1ca2
chota5511 <quannnl92@gmail.com>
2022-03-02 05:40:14 +0000

    Remove system-sleep Rebuild index.js Rebuild s3Read function

--- aws_utilities.js
+++ aws_utilities.js
@@ -1,5 +1,4 @@
 const AWS = require('aws-sdk');
-const sleep = require('system-sleep')
 
 const AWS_CREDENTIAL = {
   accessKeyId: process.env['awsAccessID'],
@@ -22,7 +21,7 @@ function s3Upload(inputStream, bucket, s3Key) {
   });
 }
 
-function s3Read(bucket, s3Key) {
+async function s3Read(bucket, s3Key) {
   const s3 = new AWS.S3(AWS_CREDENTIAL);
 
   const params = {
@@ -33,26 +32,10 @@ function s3Read(bucket, s3Key) {
   var tmp = null;          //Default return null
   var timeout = 10;
 
-  const data = s3.getObject(params, (s3Err, data) => {
-    if (s3Err) throw s3Err
-    //console.log(data)
+  const data = await s3.getObject(params).promise();
 
-    tmp = new Buffer.from(data["Body"]).toString('utf8');
-  })
-
-  //Make thing sync
-  do {
-    if (tmp != null) {
-      break;
-    }
-    else {
-      timeout--;
-      sleep(1000);
-    }
-  } while(timeout > 0);
-  if (timeout <= 0) {
-    console.log('Request timeout!!!');
-  }
+  tmp = Buffer.from(data['Body']).toString();
+  
   return tmp;
 }
 
